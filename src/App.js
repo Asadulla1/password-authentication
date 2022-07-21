8d3a04606fb3c7427a5f5da58a56d07c59374301
Riyad Nahin <riyadnahin1@gmail.com>
2022-07-21 19:45:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import initializeAuthentication from "./Firebase/firebase.init";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
 
 function App() {
+  initializeAuthentication();
+  const auth = getAuth();
+  const [newUser, setNewUser] = useState(false);
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: "",
+    email: "",
+    password: "",
+    photo: "",
+    error: "",
+    success: false,
+  });
+
+  const handleBlur = (e) => {
+    let isFormValid = true;
+    if (e.target.name === "email") {
+      isFormValid = /\S+@\S+\.\S+/.test(e.target.value);
+    }
+    if (e.target.name === "password") {
+      const isPasswordValid = e.target.value.length > 6;
+      const isPasswordHasNumber = /\d{1}/.test(e.target.value);
+      isFormValid = isPasswordValid && isPasswordHasNumber;
+    }
+    if (isFormValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+    }
+  };
+  const handleSubmit = (e) => {
+    console.log(user.email, user.password);
+    if (newUser && user.email && user.password) {
+      createUserWithEmailAndPassword(auth, user.email, user.password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          const newUserInfo = { ...user };
+          newUserInfo.error = " ";
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+          updateUserName(newUserInfo.name);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+          // console.log(errorCode, errorMessage);
+          // ..
+        });
+    }
+    if (!newUser && user.email && user.password) {
+      signInWithEmailAndPassword(auth, user.email, user.password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          const newUserInfo = { ...user };
+          newUserInfo.error = " ";
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+          console.log("signed in user info:", userCredential.user);
+          // ...
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+        });
+    }
+    e.preventDefault();
+
+    const updateUserName = (name) => {
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      })
+        .then(() => {
+          // Profile updated!
+          // ...
+        })
+        .catch((error) => {
+          // An error occurred
+          // ...
+        });
+    };
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Our Own Authentication</h1>
+      <input
+        type="checkbox"
+        name="newUser"
+        onChange={() => setNewUser(!newUser)}
+        id=""
+      />
+      <label htmlFor="newUser">New User Sign Up</label>
+      <form action="" onSubmit={handleSubmit}>
+        {newUser && <input type="text" placeholder="Write your Name" />} <br />
+        <input
+          type="text"
+          name="email"
+          placeholder="Write your Email Address"
+          required
+          onBlur={handleBlur}
+        />
+        <br />
+        <input
+          type="password"
+          name="password"
+          id=""
+          placeholder="Write your Password"
+          onBlur={handleBlur}
+        />{" "}
+        <br />
+        <input type="submit" value="Submit" />
+        <br />
+        <p style={{ color: "red" }}>{user.error}</p>
+        {user.success && (
+          <p style={{ color: "green" }}>
+            User {newUser ? "Created" : "Logged In"} Successfully{" "}
+          </p>
+        )}
+      </form>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDl3ADeVDmQ3u_66cwppLMgbaht5b70hRM",
+  authDomain: "email-pass-auth-416f6.firebaseapp.com",
+  projectId: "email-pass-auth-416f6",
+  storageBucket: "email-pass-auth-416f6.appspot.com",
+  messagingSenderId: "898891849118",
+  appId: "1:898891849118:web:a063f449a63f51f93b8a8f",
+};
+export default firebaseConfig;
